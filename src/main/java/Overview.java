9982b3a16e63f0cd8a3c1cdd1843043ec4166ec5
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-11-10 17:11:55 +0100

    started implementing staff list

--- src/main/java/Overview.java
+++ src/main/java/Overview.java
@@ -1,15 +1,22 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Overview extends JFrame{
     private JButton addSalesmanButton;
     private JButton removeSalesmanButton;
     private JTable staffTable;
+    private DefaultTableModel tm;
     private JPanel mainPanel;
     private JButton refreshButton;
 
@@ -17,7 +24,20 @@ public class Overview extends JFrame{
         super("Overview");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
+
+        tm = new DefaultTableModel(){
+          public boolean isCellEditable(int row, int column){
+              return false;
+          }
+        };
+        staffTable.setModel(tm);
+        tm.addColumn("ID");
+        tm.addColumn("Firstname");
+        tm.addColumn("Lastname");
+        staffTable.getTableHeader().resizeAndRepaint();
+
         this.pack();
+
         /*Image resource1 = ImageIO.read(new File("src//icons//add_folder.svg"));
         Icon icon1 = new ImageIcon(resource1);
         button1.setIcon(icon1);*/
@@ -28,5 +48,18 @@ public class Overview extends JFrame{
                 addFrame.setVisible(true);
             }
         });
+
+        //displaySalesMen(Arrays.asList(new SalesMan[]{new SalesMan("Jonas", "Brill", 123)}));
+        loadStaff();
+    }
+
+    public void displaySalesMen(List<SalesMan> sm){
+        for(SalesMan salesMan : sm){
+            tm.addRow(new Object[]{Integer.toString(salesMan.getId()), salesMan.getFirstname(), salesMan.getLastname()});
+        }
+    }
+
+    public void loadStaff(){
+        displaySalesMen(Main.mP.querySalesMan("123", "id"));
     }
 }
