d302e8a6a250aabce132a27ef6b88d013eab40bc
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-11-11 11:52:00 +0100

    reorganized layout

--- src/main/java/StaffDetails.java
+++ src/main/java/StaffDetails.java
@@ -1,6 +1,8 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +13,7 @@ public class StaffDetails extends JFrame{
     private JLabel id;
     private JLabel firstname;
     private JLabel lastname;
+    private JButton addRecod;
     private DefaultTableModel tm;
 
     public StaffDetails(SalesMan sm){
@@ -26,13 +29,29 @@ public class StaffDetails extends JFrame{
             }
         };
         recordsTable.setModel(tm);
-        tm.addColumn("ID");
-        tm.addColumn("Firstname");
-        tm.addColumn("Lastname");
+        tm.addColumn("Wert");
         recordsTable.getTableHeader().resizeAndRepaint();
 
         id.setText(Integer.toString(sm.getId()));
         firstname.setText(sm.getFirstname());
         lastname.setText(sm.getLastname());
+        loadRecords(sm);
+
+        addRecod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame addEvalFrame = new AddEvalRecord(sm);
+                addEvalFrame.setVisible(true);
+            }
+        });
+    }
+
+    private void loadRecords(SalesMan sm){
+        for (int i = tm.getRowCount() - 1; i >= 0; i--) {
+            tm.removeRow(i);
+        }
+        for(EvaluationRecord record : Main.mP.readEvaluationRecords(sm.getId())){
+            tm.addRow(new Object[]{record.getPerformance()});
+        }
     }
 }
