b95ebf23e9e50beab0fb862af8151447f45e5610
Jonas <jonas@rbri.de>
2020-11-15 14:11:27 +0100

    fixed warnings in class ManagePersonalInterface

--- src/main/java/ManagePersonalInterface.java
+++ src/main/java/ManagePersonalInterface.java
@@ -2,21 +2,21 @@ import java.util.List;
 
 public interface ManagePersonalInterface {
 
-    public void createSalesMan( SalesMan record );
+    void createSalesMan(SalesMan record);
     
-    public SalesMan readSalesMan( int sid );
+    SalesMan readSalesMan( int sid );
     
-    public List<SalesMan> querySalesMan(String attribute , String key );
+    List<SalesMan> querySalesMan(String attribute , String key );
 
-    public List<SalesMan> getAllSalesMen();
+    List<SalesMan> getAllSalesMen();
     
     void updateSalesMen(int sid, String key, String attribute);
 
     void deleteSalesMen(int sid);
     
-    public void addPerformanceRecord( EvaluationRecord record , int sid );
+    void addPerformanceRecord( EvaluationRecord record , int sid );
     
-    public List<EvaluationRecord> readEvaluationRecords( int sid );
+    List<EvaluationRecord> readEvaluationRecords( int sid );
 
     void updateEvaluationRecord(int id, int year, String key, int attribute);
 
