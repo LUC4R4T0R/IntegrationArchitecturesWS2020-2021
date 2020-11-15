5b314c9400a092fd39b67ea7da0a2d699466c836
Jonas <jonas@rbri.de>
2020-11-15 14:49:43 +0100

    commentet SalesManRecord-class

--- src/main/java/SalesManRecord.java
+++ src/main/java/SalesManRecord.java
@@ -1,35 +1,50 @@
 import org.bson.Document;
 
+/**
+ * @author jbrill2s, lringh2s
+ * <p>
+ * Helper-class to insert a record and its the SalesMan-id into the records-collection.
+ */
 public class SalesManRecord {
 
-	private int salesmanId;
-	private EvaluationRecord perfromance;
+    /**
+     * The id of the SalesMan, the record s about.
+     */
+    private final int salesmanId;
 
-	public SalesManRecord(int salesmanId, EvaluationRecord perfromance) {
-		this.salesmanId = salesmanId;
-		this.perfromance = perfromance;
-	}
+    /**
+     * The performance-record for the SalesMan.
+     */
+    private final EvaluationRecord perfromance;
 
-	public void setSalesmanId(int salesmanId) {
-		this.salesmanId = salesmanId;
-	}
+    /**
+     * Constructor, that creates a SalesManRecord-Object with the given attributes.
+     *
+     * @param salesmanId  The id of the SalesMan, the record s about
+     * @param perfromance The performance-record for the SalesMan.
+     */
+    public SalesManRecord(int salesmanId, EvaluationRecord perfromance) {
+        this.salesmanId = salesmanId;
+        this.perfromance = perfromance;
+    }
 
-	public EvaluationRecord getPerfromance() {
-		return perfromance;
-	}
-
+    /**
+     * This method takes this SalesManRecord and puts its attributes into a Document.
+     *
+     * @return Returns the Document witch is build out of this SalesManRecords attributes.
+     */
     public Document toDocument() {
-    	Document d = new Document();
-		d.append("LC", this.perfromance.getPerformance()[0]);
-		d.append("OtE", this.perfromance.getPerformance()[1]);
-		d.append("SBtE", this.perfromance.getPerformance()[2]);
-		d.append("AtC", this.perfromance.getPerformance()[3]);
-		d.append("CS", this.perfromance.getPerformance()[4]);
-		d.append("ItC", this.perfromance.getPerformance()[5]);
-		d.append("id", this.salesmanId);
-		int year = this.perfromance.getYear();
-		d.append("year", year);
-    	return d;
+        Document d = new Document();
+        d.append("LC", this.perfromance.getPerformance()[0]);
+        d.append("OtE", this.perfromance.getPerformance()[1]);
+        d.append("SBtE", this.perfromance.getPerformance()[2]);
+        d.append("AtC", this.perfromance.getPerformance()[3]);
+        d.append("CS", this.perfromance.getPerformance()[4]);
+        d.append("ItC", this.perfromance.getPerformance()[5]);
+        d.append("id", this.salesmanId);
+        int year = this.perfromance.getYear();
+        d.append("year", year);
+        return d;
     }
 
 }
