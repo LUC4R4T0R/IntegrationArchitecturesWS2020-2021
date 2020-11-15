0b93491437fa0c7134b7b1ad6b2e23800cc1ceef
Jonas <jonas@rbri.de>
2020-11-15 10:50:26 +0100

    code commented

--- src/main/java/EvaluationRecord.java
+++ src/main/java/EvaluationRecord.java
@@ -1,16 +1,42 @@
+/**
+ * 
+ * @author jbrill2s, lringh2s
+ * 
+ *         Class that defines an EvaluationRecord.
+ * 
+ */
 public class EvaluationRecord {
-	
+
+	/**
+	 * The evaluation for this record.
+	 */
 	private String performance;
-	
+
+	/**
+	 * Constructor, that creates an EvaluationRecord-Object with the given
+	 * performance.
+	 */
 	public EvaluationRecord(String performance) {
 		this.performance = performance;
 	}
-	
+
+	/**
+	 * Gets the evaluation of this EvaluationRecord.
+	 * 
+	 * @return Returns the performance-String.
+	 */
 	public String getPerformance() {
 		return performance;
 	}
-	
-    public boolean equals(EvaluationRecord s) {
-    	return this.performance.equals(s.getPerformance());
-    }
+
+	/**
+	 * Compares an EvaluationRecord with this one.
+	 * 
+	 * @param er The EvaluationRecord to compare with.
+	 * 
+	 * @return Returns true if the two EvaluationRecords are equal and false if not.
+	 */
+	public boolean equals(EvaluationRecord er) {
+		return this.performance.equals(er.getPerformance());
+	}
 }
