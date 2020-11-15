08ba58b54a1be5cc7fe7a984f7c96db05c56c320
Jonas <jonas@rbri.de>
2020-11-15 14:40:28 +0100

    commentet SalesMan-class

--- src/main/java/SalesMan.java
+++ src/main/java/SalesMan.java
@@ -1,77 +1,94 @@
 import org.bson.Document;
 
 /**
- * 
  * @author jbrill2s, lringh2s
- * 
- *         Class that provides the Methods to create, read, delete and update
- *         the MongoDB entries of the salesmen and there personal records.
- * 
+ * <p>
+ * Class that provides the Methods to create, read, delete and update
+ * the MongoDB entries of the salesmen and there personal records.
  */
 public class SalesMan {
-	
-	/**
-	 * The MongoDB-Collection for the salesmen.
-	 */
-	private int id;
-	private String firstname;
-	private String lastname;
 
-	/**
-	 * Constructor, that creates a ManagePersonal-Object with the given salesmen and
-	 * record MongoDB-Collection.
-	 */
-	public SalesMan(String firstname, String lastname, int id) {
-		this.firstname = firstname;
-		this.lastname = lastname;
-		this.id = id;
-	}
+    /**
+     * The MongoDB-Collection for the salesmen.
+     */
+    private int id;
+    private final String firstname;
+    private final String lastname;
 
-	/**
-	 * Inserts a new Salesman into the database.
-	 * 
-	 * @param record The SalesMan that should be inserted into the database.
-	 * 
-	 * @return This method has no return-value.
-	 */
-	public int getId() {
-		return id;
-	}
+    /**
+     * Constructor, that creates a SalesMan-Object with the given attributes.
+     */
+    public SalesMan(String firstname, String lastname, int id) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    /**
+     * Returns the id of this SalesMan.
+     *
+     * @return This method has no return-value.
+     */
+    public int getId() {
+        return id;
+    }
 
-	public String getFirstname() {
-		return firstname;
-	}
+    /**
+     * Sets the id of this SalesMan.
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    /**
+     * Returns the firstname of this SalesMan.
+     *
+     * @return Returns the firstname of this SalesMan.
+     */
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public String getLastname() {
-		return lastname;
-	}
+    /**
+     * Returns the lastname of this SalesMan.
+     *
+     * @return Returns the lastname of this SalesMan.
+     */
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
 
-	public String toString() {
-		return id + ": " + firstname + " " + lastname;
-	}
+    /**
+     * Method to display a SalesMan cleanly.
+     *
+     * @return Returns a readable String that describes the SalesMan.
+     */
+    public String toString() {
+        return id + ": " + firstname + " " + lastname;
+    }
 
-	public Document toDocument() {
-		Document d = new Document();
-		d.append("firstname", this.firstname);
-		d.append("lastname", this.lastname);
-		d.append("id", this.id);
-		return d;
-	}
+    /**
+     * This method takes this SalesMan and puts its attributes into a Document.
+     *
+     * @return Returns the Document witch is build out of this SalesMans attributes.
+     */
+    public Document toDocument() {
+        Document d = new Document();
+        d.append("firstname", this.firstname);
+        d.append("lastname", this.lastname);
+        d.append("id", this.id);
+        return d;
+    }
 
-	public boolean equals(SalesMan s) {
-		return this.lastname.equals(s.getLastname()) && this.firstname.equals(s.getFirstname()) && this.id == s.getId();
-	}
+    /**
+     * Compares this SalesMan to the given one.
+     *
+     * @param s The SalesMan to compare with.
+     * @return Returns true if the SalesMen are equal and false if not.
+     */
+    public boolean equals(SalesMan s) {
+        return this.lastname.equals(s.getLastname()) && this.firstname.equals(s.getFirstname()) && this.id == s.getId();
+    }
 
 }
